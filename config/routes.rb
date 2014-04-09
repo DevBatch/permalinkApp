64b644e963082291f94794a08ea86b2e9b553d4d
Asnad <asnad.atta@devbatch.com>
2014-04-09 19:44:33 +0500

    friendly now in place

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 PermaNew::Application.routes.draw do
+  root :to=> "posts#index"
   resources :posts
 
   # The priority is based upon order of creation: first created -> highest priority.
---
+++ db/migrate/20140409144129_add_slug.rb
@@ -0,0 +1,7 @@
+class AddSlug < ActiveRecord::Migration
+  def change
+    add_column :posts, :slug, :string
+    add_index :posts, :slug
+    Post.find_each(&:save)
+  end
+end
